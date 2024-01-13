d8712a96bf84147eac333c78a28699cff59a99c1
蕉驴（吕子琦） <LzqLeisure@gmail.com>
2024-01-13 16:02:05 +0800

    摄像机优化

---
+++ Assets/Sandbox/jiaolv_Test/Scripts/Door.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]private float RoomSize = 20f;
+    [SerializeField]private CameraController cam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.transform.position.x < transform.position.x)
+            {
+                cam.MoveToNewRoom(RoomSize);
+            }
+            else
+            {
+                cam.MoveToNewRoom(-RoomSize);
+            }
+        }
+    }
+}
